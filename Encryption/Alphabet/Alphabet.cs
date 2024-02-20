37268cc7e2c36588438340b8e1d2f1287be507f8
Egor Pankratiev <112758007+Riborok@users.noreply.github.com>
2024-02-20 16:12:36 +0300

    added alphabet

---
+++ Encryption/Alphabet/Alphabet.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Encryption.Alphabet
+{
+    public static class EnglishAlphabet
+    {
+        public static bool Contains(char c)
+        {
+            return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+        }
+        
+        public static string RemoveNonAlphabetic(string s)
+        {
+            return new string(s.Where(Contains).ToArray());
+        }
+    }
+}
